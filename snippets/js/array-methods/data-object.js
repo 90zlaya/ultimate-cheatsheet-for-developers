2fb7801031216854ae3750f5008435b987bb86e0
90zlaya <contact@zlatanstajic.com>
2021-10-05 20:03:02 +0200

    Removing number of championships from array methods data object :cowboy_hat_face:

--- snippets/js/array-methods/data-object.js
+++ snippets/js/array-methods/data-object.js
@@ -3,31 +3,26 @@ module.exports = [
     {
         name: 'Michael Schumacher',
         country: 'Germany',
-        championships: 7,
         winning_seasons: [1994, 1995, 2000, 2001, 2002, 2003, 2004]
     },
     {
         name: 'Lewis Hamilton',
         country: 'Great Britain',
-        championships: 7,
         winning_seasons: [2008, 2014, 2015, 2017, 2018, 2019, 2020]
     },
     {
         name: 'Juan Manuel Fangio',
         country: 'Argentina',
-        championships: 5,
         winning_seasons: [1951, 1954, 1955, 1956, 1957]
     },
     {
         name: 'Alain Prost',
         country: 'France',
-        championships: 4,
         winning_seasons: [1985, 1986, 1989, 1993]
     },
     {
         name: 'Sebastian Vettel',
         country: 'Germany',
-        championships: 4,
         winning_seasons: [2010, 2011, 2012, 2013]
     }
 ];
