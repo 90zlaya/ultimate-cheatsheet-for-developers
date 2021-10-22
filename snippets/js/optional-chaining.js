bc12719d46161c205cb474424fcba581e5fdfe73
90zlaya <contact@zlatanstajic.com>
2021-10-22 10:57:29 +0200

    Documented optional-chaining.js snippet :100:

--- snippets/js/optional-chaining.js
+++ snippets/js/optional-chaining.js
@@ -1,3 +1,11 @@
+/**
+ * Checking what's going to happen when
+ * optional chaining is used on example object
+ * 
+ * @author Zlatan Stajic <contact@zlatanstajic.com>
+ * @since 2021-03-01
+ */
+
 const rider = {
     name: {
         first: 'Valentino',
