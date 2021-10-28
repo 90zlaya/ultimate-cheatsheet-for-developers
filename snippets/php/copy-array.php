01c72a43b12aa4336619fe1bca3492a6befade83
90zlaya <contact@zlatanstajic.com>
2021-10-28 21:19:02 +0200

    Documented copy-array.php snippet :cowboy_hat_face:

--- snippets/php/copy-array.php
+++ snippets/php/copy-array.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Copying integers array to the new array
+ * and adding new integers to it
+ * 
+ * @author Zlatan Stajic <contact@zlatanstajic.com>
+ * @since 2021-10-22
+ */
 
 $originalNumbers = [1, 2, 3, 4, 5, 6, 7];
 $allNumbers = [...$originalNumbers, 8, 9]; // PHP 7.4 syntax
